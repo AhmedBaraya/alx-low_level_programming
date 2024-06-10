5a56b18114706bf2046f11cacf14a00fdeb46aa7
AhmedBaraya <ahmedbaraya68@gmail.com>
2024-06-10 01:12:56 -0700

    u

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,13 @@
 #include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19"
+ * to the standard error.
+ *
+ * Return: Always 1 (Error code)
+ */
 int main(void)
 {
     char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
